ee69ca6086fdba3f4509f101aa21b38723503f33
Andrea Manzini <ilmanzo@gmail.com>
2025-03-28 11:09:07 +0100

    parse gdb output

--- analyze.py
+++ analyze.py
@@ -4,20 +4,39 @@ import sys
 
 
 def parse_annotated_execution(file):
-    #TODO dummy values
+    #parse the callgrind annotate output and returns a tuple with
+    #(number of functions executed, number of lines executed)
     return (1, 4)
 
-
-if len(sys.argv) < 2:
-    print("Usage: analyze.py <annotated_file> <gdb_functions_file>")
+def parse_gdb_output(binary_name, file):
+    relevant=False
+    pattern="/usr/src/debug/" + binary_name
+    functions=0
+    while line := file.readline():
+        line=line.strip()
+        if pattern in line:
+            relevant=True
+            continue
+        if relevant:
+            if not line:
+                relevant=False
+                continue
+            functions+=1
+    return functions
+            
+if len(sys.argv) < 3:
+    print("Usage: analyze.py <binary_name> <annotated_file> <gdb_functions_file>")
     sys.exit(1)
 
+binary_name=sys.argv[1]
 
-with open(sys.argv[1]) as f:
+with open(sys.argv[2]) as f:
     executed_funcs, executed_lines=parse_annotated_execution(f)
 
+with open(sys.argv[3]) as f:
+    total_funcs=parse_gdb_output(binary_name,f)
+
 # TODO dummy values. Parse gdb output and count total lines of code
-total_funcs = 10
 total_lines = 100
 
 func_coverage = 100*executed_funcs/total_funcs
