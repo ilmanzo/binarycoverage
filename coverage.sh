1e759ec616df873275be5a9a42646f8bf2a3b72f
Andrea Manzini <ilmanzo@gmail.com>
2025-03-30 17:00:50 +0200

    changed license to GPL3 ; check for dependencies in cov script

--- coverage.sh
+++ coverage.sh
@@ -9,16 +9,20 @@ if [[ -z "$TEMP_DIR" ]]; then
   echo "Error: Failed to create temporary directory."
   exit 1
 fi
-
-# TODO: ensure tools are installed or give error
-
-
+# ensure tools are installed or give error
+for program in valgrind gdb python3; do
+  if ! command -v $program &> /dev/null; then
+    echo "Please install $program to continue"
+    exit 1
+  fi
+done
+  
 valgrind --tool=callgrind --trace-children=yes --callgrind-out-file=$TEMP_DIR/callgrind.%p pytest 2> /dev/null
 # annotate all the files
 for f in $TEMP_DIR/callgrind.* 
 do 
   base=$(basename $f)
-  # auto annotation with --context=0 can be useful for have precise source code line execution
+  # auto annotation with --context=0 can be useful to have precise source code line execution
   callgrind_annotate --auto=yes --context=0 $f > $TEMP_DIR/"${base#*.}".log 2>/dev/null
 done
 # dump all the functions in the binary
--- test_gzip.py
+++ test_gzip.py
@@ -2,16 +2,18 @@
 import os,re
 from subprocess import run
 
+PROGRAM='/usr/bin/gzip'
+
 # program should display help
 def test_help(capfd):
-    process=run(['/usr/bin/gzip','-h'])
+    process=run([PROGRAM,'-h'])
     stdout, stderr = capfd.readouterr()     
     assert process.returncode == 0
     assert "Usage:" in stdout 
 
 # program should display version information
 def test_version(capfd):
-    process=run(['/usr/bin/gzip','-V'])
+    process=run([PROGRAM,'-V'])
     stdout, stderr = capfd.readouterr()
     assert process.returncode == 0
     assert "This is free software" in stdout 
@@ -19,12 +21,12 @@ def test_version(capfd):
 
 # program should fail when given a non existing file
 def test_compress_non_existent():
-    process=run(['/usr/bin/gzip','foobar'])
+    process=run([PROGRAM,'foobar'])
     assert process.returncode==1
 
 # program should display a license
 def test_license(capfd):
-    process=run(['/usr/bin/gzip','--license'])
+    process=run([PROGRAM,'--license'])
     assert process.returncode==0
     stdout, stderr = capfd.readouterr()
     assert 'the GNU General Public License' in stdout
@@ -36,10 +38,11 @@ def test_compress_decompress(capfd):
     create_test_file(SAMPLE_FILE)
     with open(SAMPLE_FILE) as file:
         content=file.readlines()
-    process=run(['/usr/bin/gzip',SAMPLE_FILE])
+    process=run([PROGRAM,SAMPLE_FILE])
     assert process.returncode == 0
     compressed_file=SAMPLE_FILE+".gz"
-    process=run(['/usr/bin/gzip','-d',compressed_file])
+    # decompress and read back content
+    process=run([PROGRAM,'-d',compressed_file])
     assert process.returncode == 0
     with open(SAMPLE_FILE) as file:
         assert(file.readlines()==content)
@@ -48,14 +51,15 @@ def test_compress_decompress(capfd):
 # program should give error on a damaged compressed file
 def test_decompress_error(capfd):
     wrong_file='dummy.txt'
-    wrong_compressed=wrong_file+'.gz'
     create_test_file(wrong_file)
-    process=run(['/usr/bin/gzip',wrong_file])
-    # now damage the file, decompression should fail
+    wrong_compressed=wrong_file+'.gz'
+    process=run([PROGRAM,wrong_file])
+    # now damage the compressed file by writing a random byte
     with open(wrong_compressed, "r+b") as file:
         file.seek(32)
         file.write(bytes(0xFF))
-    process=run(['/usr/bin/gzip','-d',wrong_compressed])
+    # decompression should fail        
+    process=run([PROGRAM,'-d',wrong_compressed])
     stdout, stderr = capfd.readouterr()
     assert process.returncode==1
     assert 'invalid compressed data' in stderr
