1ffc53e31b2d681088ca587e1287e3a50e7b6d47
Andrea Manzini <ilmanzo@gmail.com>
2025-04-08 08:39:01 +0200

    [WIP] pre-process callgrind files from python

--- calc_coverage.py
+++ calc_coverage.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 # take as input a folder with the function list (from gdb) and files from callgrind_annotate output
-import sys,re,argparse,os,glob
+import sys,argparse,os,glob,subprocess
+
 
 def gdb_extract_function_name(line):
+    "take as input a single line from gdb output, and extracts the function name"
     # Find the position of the '(' and extract the substring from the end of the line
     paren_index = line.rfind('(')
     if paren_index == -1:
@@ -17,14 +19,16 @@ def gdb_extract_function_name(line):
     function_name = function_name.split('.')[0]
     return function_name
 
+
 def callgrind_extract_function_name(line):
+    "take as input a single line from callgrind_annotate output, and extracts the function name"
     colon_index = line.find(':')
     if colon_index == -1:
         return None
     sub_line = line[colon_index+1:]
     dot_index = sub_line.find('.')
     if dot_index != -1:
-        sub_line=sub_line[:dot_index]
+        sub_line = sub_line[:dot_index]
     space_index = sub_line.find(' ')
     if space_index == -1:
         return sub_line
@@ -32,62 +36,78 @@ def callgrind_extract_function_name(line):
 
 
 def parse_annotated_execution(file, binary_name):
-    # parse the callgrind annotate output and returns a 
-    # set of functions executed
-    pattern=f"/usr/src/debug/{binary_name}"
-    executed_functions=set()
-    while line:=file.readline():
-        line=line.strip()
+    "parse the callgrind annotate output and returns a set of functions executed"
+    pattern = f"/usr/src/debug/{binary_name}"
+    executed_functions = set()
+    while line := file.readline():  # walrus operator
+        line = line.strip()
         if pattern in line:
-            func=callgrind_extract_function_name(line)
+            func = callgrind_extract_function_name(line)
             if func:
                 executed_functions.add(func)
     return executed_functions
 
+
 def parse_gdb_output(file, binary_name):
-    relevant=False
-    patterns=[f"/usr/src/debug/{binary_name}", "<artificial>"]
-    all_functions=set()
+    "process the gdb output file looking for functions"
+    relevant = False
+    all_functions = set()
     while line := file.readline():
-        line=line.strip()
+        line = line.strip()
         if line.startswith('File '):
-            relevant=True
+            relevant = True
             continue
         if relevant:
             if not line:
-                relevant=False
+                relevant = False
                 continue
-            func=gdb_extract_function_name(line)
+            func = gdb_extract_function_name(line)
             if func:
                 all_functions.add(func)
-    #print(f"DEBUG ALL: {all_functions}")
+    # print(f"DEBUG ALL: {all_functions}")
     return all_functions
 
+
 def main():
+    "program entry point"
     parser = argparse.ArgumentParser(description="Process a binary file with optional parameters.")
     parser.add_argument("--binary", "-b", required=True, help="name of the binary file [ex. gzip]")
     parser.add_argument("--coverdir", "-d", required=True, help="Path to the coverage data directory")
     parser.add_argument("--verbose", "-v", required=False, help="Output also the names of functions", action='store_true')
 
     args = parser.parse_args()
-    executed_funcs=set()
- 
-    for file in glob.glob(os.path.join(args.coverdir,'*.log')):
-        with open(file) as f:
-            ef=parse_annotated_execution(f,args.binary)
-            executed_funcs.update(ef)  # join the sets
+    executed_funcs = set()
 
-    with open(os.path.join(args.coverdir,"all_funcs.gdb")) as f:
-        total_funcs=parse_gdb_output(f,args.binary)
+    for file in glob.glob(os.path.join(args.coverdir, 'callgrind.*')):
+        try:
+            result = subprocess.run(
+                            ["callgrind_annotate", "--auto=yes", "--context=0", file],
+                            capture_output=True,  # Capture stdout and stderr
+                            text=True,  # Decode stdout and stderr as text
+                            check=True, # Raise exception on non-zero exit code.
+                        )
+            with open(result.stdout) as f:
+                ef = parse_annotated_execution(f, args.binary)
+                executed_funcs.update(ef)  # join the sets
+
+        except subprocess.CalledProcessError as e:
+            print(f"Error processing {f}: {e}")
+        except FileNotFoundError:
+            print("callgrind_annotate command not found. Please install it.")
+            sys.exit(1)
+
+    with open(os.path.join(args.coverdir, "all_funcs.gdb")) as f:
+        total_funcs = parse_gdb_output(f, args.binary)
 
     func_coverage = 100*len(executed_funcs)/len(total_funcs)
     print("--- Binary coverage report ---")
     print(f"Functions coverage: {len(executed_funcs)}/{len(total_funcs)} {func_coverage:.2f}%")   
     if args.verbose:
-        executed=",".join(sorted(list(executed_funcs)))
-        missing=",".join(sorted(list(total_funcs - executed_funcs)))
+        executed = ",".join(sorted(list(executed_funcs)))
+        missing = ",".join(sorted(list(total_funcs - executed_funcs)))
         print(f"\nExecuted functions: {executed}")
         print(f"\nMissing functions : {missing}")
+
 
 if __name__ == "__main__":
     main()
--- coverage.sh
+++ coverage.sh
@@ -18,13 +18,13 @@ for program in valgrind gdb python3; do
 done
   
 valgrind --tool=callgrind --trace-children=yes --callgrind-out-file=$TEMP_DIR/callgrind.%p pytest 2> /dev/null
-# annotate all the files
-for f in $TEMP_DIR/callgrind.* 
-do 
-  base=$(basename $f)
-  # auto annotation with --context=0 can be useful to have precise source code line execution
-  callgrind_annotate --auto=yes --context=0 $f > $TEMP_DIR/"${base#*.}".log 2>/dev/null
-done
+# annotate all the files (done by python script now)
+#for f in $TEMP_DIR/callgrind.* 
+#do 
+#  base=$(basename $f)
+#  # auto annotation with --context=0 can be useful to have precise source code line execution
+#  callgrind_annotate --auto=yes --context=0 $f > $TEMP_DIR/"${base#*.}".log 2>/dev/null
+#done
 # dump all the functions in the binary
 gdb -ex 'set pagination off' -ex 'info functions' -ex quit $(which $BINARY) > $TEMP_DIR/all_funcs.gdb
 # experimental: count lines of code
